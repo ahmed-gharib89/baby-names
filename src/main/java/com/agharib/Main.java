bf2dba1b327d31fc84083062855cd2d3d532bb92
Ahmed Gharib <a.gharib89@yahoo.com>
2022-05-02 19:36:04 +0200

    Add getName and whatIsNameInYear methods

--- src/main/java/com/agharib/Main.java
+++ src/main/java/com/agharib/Main.java
@@ -46,12 +46,46 @@ public class Main {
         return -1;
     }
 
+    public String getName(int year, int rank, String gender) {
+        int count = 0;
+        String name = "NO NAME";
+        String frName = "src/main/resources/us_babynames/us_babynames_test/yob" + year + "short.csv";
+        FileResource fr = new FileResource(frName);
+        for (CSVRecord rec : fr.getCSVParser(false)) {
+            if (rec.get(1).equals(gender)) {
+                count++;
+            }
+            if (count == rank) {
+                return rec.get(0);
+            }
+        }
+        return name;
+    }
+
+    public void whatIsNameInYear (String name, int year, int newYear, String gender) {
+        int rank = getRank(year, name, gender);
+        String newName = getName(newYear, rank, gender);
+        String sheOrHe = "";
+        if(gender.equals("F")){
+            sheOrHe = "she";
+        } else {
+            sheOrHe = "he";
+        }
+        System.out.println(name + " born in "+ year + " would be " + newName + " if " + sheOrHe +" was born in " + newYear + ".");
+    }
+
     public static void main(String[] args) {
         System.out.println("Starting the application..");
         FileResource fr = new FileResource("src/main/resources/us_babynames/us_babynames_test/example-small.csv");
         Main m = new Main();
 //        m.totalBirths(fr);
-        System.out.println("Rank of Mason and Gender M in year 2012 = " + m.getRank(2012, "Mason", "M"));
-        System.out.println("Rank of Mason and Gender F in year 2012 = " + m.getRank(2012, "Mason", "F"));
+//        System.out.println("Rank of Mason and Gender M in year 2012 = " + m.getRank(2012, "Mason", "M"));
+//        System.out.println("Rank of Mason and Gender F in year 2012 = " + m.getRank(2012, "Mason", "F"));
+//        System.out.println("The name of ranked 2 male in 2012 = " + m.getName(2012, 2, "M"));
+//        System.out.println("The name of ranked 2 female in 2013 = " + m.getName(2013, 2, "F"));
+//        System.out.println("The name of ranked 100 male in 2012 = " + m.getName(2012, 100, "M"));
+        m.whatIsNameInYear("Isabella", 2012, 2014, "F");
+        m.whatIsNameInYear("Mason", 2012, 2014, "F");
+
     }
 }
