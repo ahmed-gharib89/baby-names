e8f79be4c1a9ae357ed17a79f74bf6b554afc9d9
Ahmed Gharib <a.gharib89@yahoo.com>
2022-05-02 19:13:16 +0200

    Add getRank method

--- src/main/java/com/agharib/Main.java
+++ src/main/java/com/agharib/Main.java
@@ -3,6 +3,7 @@ package com.agharib;
 import edu.duke.FileResource;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.jetbrains.annotations.NotNull;
 
 public class Main {
     public void totalBirths(FileResource fr) {
@@ -28,12 +29,29 @@ public class Main {
         System.out.println("Total births = " + totalBirths + " and number of names = " + totalNames + " names");
         System.out.println("Total Boys = " + totalBoys + " and number of boys = " + numBoys + " boys");
         System.out.println("Total Girls = " + totalGirls + " and number of girls = " + numGirls + " girls");
-
     }
+
+    public int getRank(int year, String name, String gender) {
+        int count = 0;
+        String frName = "src/main/resources/us_babynames/us_babynames_test/yob" + year + "short.csv";
+        FileResource fr = new FileResource(frName);
+        for (CSVRecord rec : fr.getCSVParser(false)) {
+            if (rec.get(1).equals(gender)) {
+                count++;
+                if (rec.get(0).equals(name)) {
+                    return count;
+                }
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         System.out.println("Starting the application..");
         FileResource fr = new FileResource("src/main/resources/us_babynames/us_babynames_test/example-small.csv");
         Main m = new Main();
-        m.totalBirths(fr);
+//        m.totalBirths(fr);
+        System.out.println("Rank of Mason and Gender M in year 2012 = " + m.getRank(2012, "Mason", "M"));
+        System.out.println("Rank of Mason and Gender F in year 2012 = " + m.getRank(2012, "Mason", "F"));
     }
 }
